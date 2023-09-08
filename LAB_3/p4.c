1a017722bc5c25572d4242e8e8e7c4538b0ccb0e
Piyushku-jena <piyushku.jena@gmail.com>
2023-09-08 19:44:33 +0530

    minor fixes

--- LAB_3/p4.c
+++ LAB_3/p4.c
@@ -8,41 +8,35 @@ int main(){
     int matrix[r][r];
     for(int rr=0;rr<r;rr++){
         for(int cc=0;cc<r;cc++){
-            printf("Input %d row %d column element: ",rr+1,cc+1);
+            // printf("Input %d row %d column element: ",rr+1,cc+1);
             scanf("%d",&matrix[rr][cc]);
-
             if(matrix[rr][cc]>0){
             counte++;
             }
         }
-    }
-    for(int rr=0;rr<r;rr++){
-        for(int cc=0;cc<r;cc++){
-            printf("%2d ",matrix[rr][cc]);
-        }
         printf("\n");
     }
-
-    printf("Number of non-zero elements are: %d\n",counte);
-
-
+    // for(int rr=0;rr<r;rr++){
+    //     for(int cc=0;cc<r;cc++){
+    //         printf("%2d ",matrix[rr][cc]);
+    //     }
+    //     printf("\n");
+    // }
+    printf("Number of non-zero elements: %d\n",counte);
     for(int rr=0;rr<r;rr++){
         int sum=0;
         for(int cc=0;cc<r;cc++){
             sum+=matrix[rr][cc];
         }
-        printf("row %d sum: %d\n",rr+1,sum);
+        printf("Row %d, Sum=%d, average=%d\n",rr+1,sum,sum/r);
     }
-
     for(int cc=0;cc<r;cc++){
         int sum=0;
         for(int rr=0;rr<r;rr++){
             sum+=matrix[rr][cc];
         }
-        printf("column %d sum: %d\n",cc+1,sum);
+        printf("Column %d, Sum=%d, average=%d\n",cc+1,sum,sum/r);
     }
-
-
     return 0;
 }
 
---
+++ LAB_3/tempCodeRunnerFile.c
@@ -0,0 +1,6 @@
+    // for(int rr=0;rr<r;rr++){
+    //     for(int cc=0;cc<r;cc++){
+    //         printf("%2d ",matrix[rr][cc]);
+    //     }
+    //     printf("\n");
+    // }
